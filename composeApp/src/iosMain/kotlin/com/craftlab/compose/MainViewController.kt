9b93b0edc1a22b5c2cbad93a01fb05d8652abc08
yangcyzhang <yangcyzhang@gamil.com>
2026-09-10 11:27:40 +0800

    chore: update gitignore and commit necessary changes

---
+++ composeApp/src/iosMain/kotlin/com/craftlab/compose/MainViewController.kt
@@ -0,0 +1,6 @@
+package com.craftlab.compose
+
+import androidx.compose.ui.window.ComposeUIViewController
+import platform.UIKit.UIViewController
+
+fun MainViewController(): UIViewController = ComposeUIViewController { App() }
